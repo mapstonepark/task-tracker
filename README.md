6c5bd07379e4cf6c93ba9bebce2216a79887e414
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-10-17 08:32:54 -0500

    React-Tracker App

--- README.md
+++ README.md
@@ -68,3 +68,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+Currently in step Toggle Reminder around minute 58 in traversy media.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+
 
 function App() {
+  const [tasks, setTasks] = useState( [
+    {
+        id: 1,
+        text: 'Make Characters',
+        day: 'October 22nd 7pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Check on Mom on her appt',
+        day: 'October 19th 8am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Call Tanner ',
+        day: 'October 18th, 7:30am',
+        reminder: true,
+    }
+    
+
+])
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=>task.id !==id))
+}
+ 
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task)=> 
+  task.id === id ?
+   {...task, reminder:!task.reminder } : task))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Header />
+     {tasks.length > 0 ? 
+     <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }

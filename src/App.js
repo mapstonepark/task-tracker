3abe3ea9e9ea527b90610cad613782b410a9cc57
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-10-23 06:40:40 -0500

    built out, still need to add database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import AddTask from './components/AddTask'
 
 
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState 
+  (false)
   const [tasks, setTasks] = useState( [
     {
         id: 1,
@@ -50,8 +52,9 @@ const toggleReminder = (id) => {
 }
   return (
     <div className="container">
-     <Header />
-     <AddTask onAdd={addTask} />
+     <Header onAdd={() => setShowAddTask
+      (!showAddTask)} showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask} />}
      {tasks.length > 0 ? (
      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> ): ('No Tasks To Show')
      }
